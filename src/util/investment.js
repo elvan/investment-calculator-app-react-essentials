0ec4c303b4d505d1f4975c4b729c78a06b193b7c
Elvan Hidayat <elvanhd@gmail.com>
2023-12-02 12:35:55 +0700

    strict mode

--- src/util/investment.js
+++ src/util/investment.js
@@ -4,27 +4,22 @@
 // - annualInvestment: The amount invested every year
 // - expectedReturn: The expected (annual) rate of return
 // - duration: The investment duration (time frame)
-export function calculateInvestmentResults({
-  initialInvestment,
-  annualInvestment,
-  expectedReturn,
-  duration,
-}) {
-  const annualData = [];
+export function calculateInvestmentResults(
+  { initialInvestment, annualInvestment, expectedReturn, duration },
+  results,
+) {
   let investmentValue = initialInvestment;
 
   for (let i = 0; i < duration; i++) {
     const interestEarnedInYear = investmentValue * (expectedReturn / 100);
     investmentValue += interestEarnedInYear + annualInvestment;
-    annualData.push({
+    results.push({
       year: i + 1, // year identifier
       interest: interestEarnedInYear, // the amount of interest earned in this year
       valueEndOfYear: investmentValue, // investment value at end of year
       annualInvestment: annualInvestment, // investment added in this year
     });
   }
-
-  return annualData;
 }
 
 // The browser-provided Intl API is used to prepare a formatter object
