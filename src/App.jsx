367eb67e1dca953a3401edefd906359bbc27ac38
Elvan Hidayat <elvanhd@gmail.com>
2024-05-01 21:00:55 +0700

    Refactor state management and add Results component
    - Move state management for user inputs to App.jsx for shared state
    - Modify UserInput component to accept props for user state and
      change handler
    - Implement Results component to display calculation results
    - Link Results component to display data based on user inputs
    - Ensure all user input changes are propagated through new
      centralized state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from 'react';
 import Header from './components/Header.jsx';
+import Results from './components/Results.jsx';
 import UserInput from './components/UserInput.jsx';
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
     </>
   );
 }
