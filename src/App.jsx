b4618160134b2ae98d5fcc3c9553ccacb988b18a
Elvan Hidayat <elvanhd@gmail.com>
2024-05-01 20:40:22 +0700

    Enhance App with Header and UserInput components
    - Integrate Header and UserInput components into App.jsx
    - Create Header component with logo and title
    - Develop UserInput component to manage investment details
    - Implement state management in UserInput for dynamic updates
    - Configure initial and annual investment, expected return, and
      duration inputs with handleChange method

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Header from './components/Header.jsx';
+import UserInput from './components/UserInput.jsx';
+
 function App() {
-  return <h1>React Investment Calculator</h1>;
+  return (
+    <>
+      <Header />
+      <UserInput />
+    </>
+  );
 }
 
 export default App;
