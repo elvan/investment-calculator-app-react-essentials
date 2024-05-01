e7e712b69ef5c406af0e80b00c3136f34045394e
Elvan Hidayat <elvanhd@gmail.com>
2024-05-01 21:34:51 +0700

    Validate input and update results display logic
    - Add validation to ensure duration input is greater than zero
    - Display error message if the input duration is invalid
    - Integrate detailed results display in Results component
    - Format investment results data using the currency formatter
    - Calculate and display total interest and invested capital

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -24,7 +26,8 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && <p className='center'>Please enter a duration greater than zero.</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
